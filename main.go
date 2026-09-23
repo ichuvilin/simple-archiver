6ecb148cd97980dccdca7ed8005233775013b19b
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 14:35:28 +0500

    impl encode

--- main.go
+++ main.go
@@ -52,5 +52,63 @@ func (sa *SimpleArchiver) createControlByte(count int, isCompressed bool) byte {
 	return byte(count)
 }
 
+func (sa *SimpleArchiver) encode(data []byte) []byte {
+	result := make([]byte, 0)
+
+	for i := 0; i < len(data); {
+		repeatCount := countRepeating(data, i)
+
+		if repeatCount >= 4 {
+			count := repeatCount
+
+			if count > 127 {
+				count = 127
+			}
+
+			result = append(
+				result,
+				sa.createControlByte(count, true),
+			)
+			result = append(result, data[i])
+
+			i += count
+			continue
+		}
+
+		start := i
+		length := 0
+
+		for i < len(data) && length < 127 {
+			repeatCount = countRepeating(data, i)
+
+			if repeatCount >= 3 && length > 0 {
+				break
+			}
+
+			i++
+			length++
+		}
+
+		result = append(
+			result,
+			sa.createControlByte(length, false),
+		)
+		result = append(result, data[start:i]...)
+	}
+
+	return result
+}
+
+func countRepeating(data []byte, start int) int {
+	count := 1
+
+	for start+count < len(data) &&
+		data[start] == data[start+count] {
+		count++
+	}
+
+	return count
+}
+
 func main() {
 }
