ed52c7ac499fa352ae1b36094e43fcd50b7769ad
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 15:34:28 +0500

    impl menu

--- main.go
+++ main.go
@@ -343,7 +343,43 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
+func (m model) updateMenu(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
+	switch msg.String() {
+	case "up", "k":
+		if m.cursor > 0 {
+			m.cursor--
+		}
+
+	case "down", "j":
+		if m.cursor < len(m.choices)-1 {
+			m.cursor++
+		}
+
+	case "enter":
+		switch m.cursor {
+		case 0:
+			m.state = "compress"
+		case 1:
+			m.state = "decompress"
+		case 2:
+			return m, tea.Quit
+		}
+
+	case "q", "ctrl+c":
+		return m, tea.Quit
+	}
+
+	return m, nil
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if m.state == "menu" {
+			return m.updateMenu(msg)
+		}
+	}
+
 	return m, nil
 }
 
