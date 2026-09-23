36b863cba2a23f5905218413bf01ae7605d15013
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 13:52:10 +0500

    impl compressEmpty

--- main.go
+++ main.go
@@ -13,6 +13,14 @@ func NewArchiver(inputPath string) *SimpleArchiver {
 	}
 }
 
+func (sa *SimpleArchiver) compressEmpty(data []byte) []byte {
+	if len(data) == 0 {
+		return []byte{}
+	}
+
+	return data
+}
+
 func main() {
 
 }
