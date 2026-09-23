9b85df22ec857c5889522bee1e2a7ef80097f090
Ivan Chuvilin <chuvilin_i@joy.money>
2026-09-23 13:50:18 +0500

    create SimpleArchiver

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+type SimpleArchiver struct {
+	inputPath  string
+	outputPath string
+	buffer     []byte
+}
+
+func NewArchiver(inputPath string) *SimpleArchiver {
+	return &SimpleArchiver{
+		inputPath: inputPath,
+		buffer:    make([]byte, 1024*8),
+	}
+}
+
 func main() {
 
 }
